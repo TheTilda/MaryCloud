fb20c4a601d5babf963bb52710d9a71fa0509f01
Vasya Pupkin <segasl142@mail.ru>
2023-08-15 20:44:24 +0700

    по html, началась работа по созданию выгрузки фото из file_id

--- html/flas.py
+++ html/flas.py
@@ -2,21 +2,25 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import InputFile
 from aiogram.utils import executor
-
-from flask import render_template, request
+from flask import Flask, render_template, request
 from database import Database
 
-app = Flask(__name__)
+
 db = Database()
 
+app = Flask(__name__)
 @app.route('/upload', methods=['POST'])
 def upload():
     file_id = request.form['file_id']
 
-    
     db.upload_file(file_id)
   
-    return render_template('html/index.html', file_id=file_id)
+    return render_template('index.html', file_id=file_id)
+
+@app.route('/')
+def home():
+    name = 'John'
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run()
